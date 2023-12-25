85eb3984e8d918a11678cbb3a99c6e47d8fa6f89
thapanankulab25 <s6503052422108@email.kmutnb.ac.th>
2023-12-25 09:59:19 +0700

    test01

---
+++ app1/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://stickershop.line-scdn.net/stickershop/v1/product/1130832/LINEStorePC/main.png?v=1 " className="App-logo" alt="logo" />
+        <p>
+          SEK <code> </code> LOSO
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/channel/UCA5AGm9ExHommjiDaXXGwCw  "
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ใจสั่งมา
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
