f03fe5ea1418df5d880ed2ed15f098ee063cefd6
thapanankulab25 <s6503052422108@email.kmutnb.ac.th>
2023-12-25 11:18:31 +0700

    update css

--- app1/src/App.js
+++ app1/src/App.js
@@ -13,19 +13,26 @@ function App() {
 //     )
 // return el
 
-return(
-  <table border="1">
-  <tr>
-    <th>Product</th><th>Price</th>
-    </tr>
-  <tr>
-    <td>Book</td><td>320</td>
-  </tr>
-  <tr>
-    <td>Football</td><td>50</td>
-  </tr>
-  </table>
-)
+// return(
+//   <table border="1">
+//   <tr>
+//     <th>Product</th><th>Price</th>
+//     </tr>
+//   <tr>
+//     <td>Book</td><td>320</td>
+//   </tr>
+//   <tr>
+//     <td>Cake</td><td>70</td>
+//   </tr>
+//   </table>
+// )
+
+const divStyle ={
+  color:'red',
+  backgroundcolor:'powderblue',
+  fontSize:'larger',
+}
+return <div style={divStyle}>Hello React</div>
 }
 
 export default App;
