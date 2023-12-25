aa488cd6d7c9e37b6048f575cefb0565fde4d668
thapanankulab25 <s6503052422108@email.kmutnb.ac.th>
2023-12-25 10:31:00 +0700

    edit app.js

--- app1/src/App.js
+++ app1/src/App.js
@@ -3,23 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="https://stickershop.line-scdn.net/stickershop/v1/product/1130832/LINEStorePC/main.png?v=1 " className="App-logo" alt="logo" />
-        <p>
-          SEK <code> </code> LOSO
-        </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/channel/UCA5AGm9ExHommjiDaXXGwCw  "
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ใจสั่งมา
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h3>Hello World!</h3>
+    <div className='title'>Psek</div>
+    </>
+    );
+
 }
 
 export default App;
