293d52b978f921515c90fd831dbf1f1eda300862
thapanankulab25 <s6503052422108@email.kmutnb.ac.th>
2023-12-25 11:12:21 +0700

    update table

--- app1/src/App.js
+++ app1/src/App.js
@@ -3,15 +3,29 @@ import './App.css';
 
 function App() {
 
-    let r='<b>React</b>'
-    let rn='<b>React</b> Native'
-    const el=(
-      <div>
-        Create Web <br/>
-        with <span dangerouslySetInnerHTML={{__html:r}}/> & <span dangerouslySetInnerHTML={{__html:rn}}/>
-      </div>
-    )
-return el
+//     let r='<b>React</b>'
+//     let rn='<b>React</b> Native'
+//     const el=(
+//       <div>
+//         Create Web <br/>
+//         with <span dangerouslySetInnerHTML={{__html:r}}/> & <span dangerouslySetInnerHTML={{__html:rn}}/>
+//       </div>
+//     )
+// return el
+
+return(
+  <table border="1">
+  <tr>
+    <th>Product</th><th>Price</th>
+    </tr>
+  <tr>
+    <td>Book</td><td>320</td>
+  </tr>
+  <tr>
+    <td>Football</td><td>50</td>
+  </tr>
+  </table>
+)
 }
 
 export default App;
