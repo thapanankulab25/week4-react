4a411c3ca565af23f1a32726c85e150baac81339
thapanankulab25 <s6503052422108@email.kmutnb.ac.th>
2023-12-25 11:00:29 +0700

    test</b>

--- app1/src/App.js
+++ app1/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <>
-<h3>Hello World</h3>
-<div className='title'> React</div>
-</>
-)
+
+    let r='<b>React</b>'
+    let rn='React Native'
+    const el=(
+      <div>
+        Create Web <br/>
+        with  <span dangerouslySetInnerHTML={{__html:r}}/> & {rn}
+      </div>
+    )
+return el
 }
 
 export default App;
